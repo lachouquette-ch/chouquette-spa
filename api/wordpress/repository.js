f2e753ea8b7c3f5bab4dbc1957035523e3fb142d
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-03-31 18:50:52 +0200

    axios $get shortcut

--- api/wordpress/repository.js
+++ api/wordpress/repository.js
@@ -6,25 +6,27 @@ class AbstractEndpoint {
   }
 
   constructor(resource) {
-    this.resource = resource
     this.axios = axios.create({
       baseURL: `${process.env.wpBaseUrl}/wp-json/wp/v2/${resource}`
     })
+    this.axios.$get = (...args) => {
+      return this.axios.get.apply(this, args).then((response) => response.data)
+    }
   }
 
   getBySlug(slug) {
-    const URI = `?slug=${slug}&_embed=true`
-    return this.axios.get(URI).then((response) => response.data[0])
+    const URI = `/?slug=${slug}&_embed=true`
+    return this.axios.$get(URI).then((posts) => posts[0])
   }
 
   getByIds(ids) {
     const URI = `/?include=${ids.join(',')}&per_page=${ids.length}&_embed=true`
-    return this.axios.get(URI).then((response) => response.data)
+    return this.axios.$get(URI)
   }
 
   getById(id) {
     const URI = `/${id}?_embed=true`
-    return this.axios.get(URI).then((response) => response.data)
+    return this.axios.$get(URI)
   }
 }
 
