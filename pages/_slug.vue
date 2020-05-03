91032e6db9cf953691b93dbdbf05569f95825473
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-05-03 22:07:44 +0200

    slug / pages : remove all 'hard coded' pages and use slug as a dispatcher between pages and posts

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,15 +1,45 @@
 <template>
-  <WpPost :post="post" :comments="comments" :similar-posts="similarPosts" :fiches="fiches" />
+  <div>
+    <WpPage v-if="pageType === 'page'" :page="page" />
+    <WpPost
+      v-else-if="pageType === 'post'"
+      :post="post"
+      :comments="comments"
+      :similar-posts="similarPosts"
+      :fiches="fiches"
+    />
+  </div>
 </template>
 
 <script>
+import _ from 'lodash'
+
 import WpPost from '~/components/WpPost'
+import WpPage from '~/components/WpPage'
+
 export default {
   components: {
+    WpPage,
     WpPost
   },
-  async asyncData({ app, store, params }) {
+  async asyncData({ app, store, params, error }) {
+    // first try as a page
+    const page = await app.$wpAPI.wp.pages.getBySlug(params.slug).then(({ data }) => data[0])
+    if (page) {
+      return {
+        pageType: 'page',
+        page
+      }
+    }
+
+    // then as a post
     const post = await app.$wpAPI.wp.posts.getBySlug(params.slug, { _embed: true }).then(({ data }) => data[0])
+
+    // quite if not a post neither
+    if (_.isEmpty(post)) {
+      error({ statusCode: '404', message: `'${params.slug}' n'existe pas` })
+      return {}
+    }
 
     const [comments, similarPosts, fiches] = await Promise.all([
       app.$wpAPI.wp.comments.getByPost(post.id).then(({ data }) => data),
@@ -18,10 +48,16 @@ export default {
     ])
 
     return {
+      pageType: 'post',
       post,
       comments,
       similarPosts,
       fiches
+    }
+  },
+  data() {
+    return {
+      pageType: null
     }
   }
 }
