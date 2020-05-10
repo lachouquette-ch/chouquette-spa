ebce8ddbd072b26d475c1a80696297aa0222d5da
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-05-10 22:33:57 +0200

    fiche : update share button

--- components/FicheShare.vue
+++ components/FicheShare.vue
@@ -5,38 +5,39 @@
         class="btn btn-sm btn-outline-secondary text-black"
         @click.prevent="shareWith('Fiche sur La Chouquette', escapedTitle, fichePage)"
       >
-        <span class="mr-2"><i class="fas fa-share-alt"></i></span>Partager
+        <span class="mr-2"><i class="fas fa-share-alt"></i></span>
       </button>
     </template>
     <template v-else>
-      <b-dropdown size="sm" dropup text="A partager sur" variant="outline-secondary" toggle-class="text-black">
-        <b-dropdown-item
-          :href="`https://www.facebook.com/sharer/sharer.php?u=${fichePage}`"
-          target="_blank"
-          variant="secondary"
-        >
-          <span style="color: #4267b2" class="mr-2"><i class="fab fa-facebook-square"></i></span> Facebook
-        </b-dropdown-item>
-        <b-dropdown-item
-          :href="`https://twitter.com/share?text=${escapedTitle}&url=${encodeURI(fichePage)}`"
-          target="_blank"
-          variant="secondary"
-        >
-          <span style="color: #38A1F3" class="mr-2"><i class="fab fa-twitter-square"></i></span> Twitter
-        </b-dropdown-item>
-        <b-dropdown-item
-          :href="`https://wa.me/?text=${encodeURI(fichePage)}`"
-          target="_blank"
-          variant="secondary"
-        >
-          <span style="color: #25d366" class="mr-2"><i class="fab fa-whatsapp-square"></i></span> WhatsApp
-        </b-dropdown-item>
-        <b-dropdown-item
-          :href="`mailto:?subject=${escapedTitle}&amp;body=Je te partage cet article ${fichePage}`"
-          variant="secondary"
-        >
-          <span style="color: #b7b7b7" class="mr-2"><i class="fas fa-envelope-square"></i></span> Email
-        </b-dropdown-item>
+      <b-dropdown size="sm" dropup no-caret variant="outline-secondary" toggle-class="text-black">
+        <template #button-content>
+          <span class="mx-1"><i class="fas fa-share-alt"></i></span>
+        </template>
+        <template #default>
+          <b-dropdown-item
+            :href="`https://www.facebook.com/sharer/sharer.php?u=${fichePage}`"
+            target="_blank"
+            variant="secondary"
+          >
+            <span style="color: #4267b2" class="mr-2"><i class="fab fa-facebook-square"></i></span> Facebook
+          </b-dropdown-item>
+          <b-dropdown-item
+            :href="`https://twitter.com/share?text=${escapedTitle}&url=${encodeURI(fichePage)}`"
+            target="_blank"
+            variant="secondary"
+          >
+            <span style="color: #38A1F3" class="mr-2"><i class="fab fa-twitter-square"></i></span> Twitter
+          </b-dropdown-item>
+          <b-dropdown-item :href="`https://wa.me/?text=${encodeURI(fichePage)}`" target="_blank" variant="secondary">
+            <span style="color: #25d366" class="mr-2"><i class="fab fa-whatsapp-square"></i></span> WhatsApp
+          </b-dropdown-item>
+          <b-dropdown-item
+            :href="`mailto:?subject=${escapedTitle}&amp;body=Je te partage cet article ${fichePage}`"
+            variant="secondary"
+          >
+            <span style="color: #b7b7b7" class="mr-2"><i class="fas fa-envelope-square"></i></span> Email
+          </b-dropdown-item>
+        </template>
       </b-dropdown>
     </template>
   </span>
