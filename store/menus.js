f047946a5dff2e265c8dbfcee8ec6badb689cd85
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-05-11 12:24:48 +0200

    store : cache menus

--- store/menus.js
+++ store/menus.js
@@ -11,30 +11,34 @@ export const state = () => ({
 })
 
 export const actions = {
-  async init({ commit, dispatch }) {
-    // fetch all menus
-    const rawMenus = await this.$wpAPI.menus.get().then(({ data }) => data)
-    // build menus with all dependencies
-    const menus = await Promise.all(
-      rawMenus.map((rawMenu) => this.$wpAPI.menus.getById(rawMenu.term_id).then(({ data }) => data))
-    )
+  async init({ state, commit, dispatch }) {
+    if (state.header && state.footer) {
+      return [state.header, state.footer]
+    } else {
+      // fetch all menus
+      const rawMenus = await this.$wpAPI.menus.get().then(({ data }) => data)
+      // build menus with all dependencies
+      const menus = await Promise.all(
+        rawMenus.map((rawMenu) => this.$wpAPI.menus.getById(rawMenu.term_id).then(({ data }) => data))
+      )
 
-    /* Process header menu */
-    // get menu and add to store
-    const headerMenu = menus.find((menu) => menu.slug === HEADER_SLUG)
-    commit('SET_HEADER', headerMenu)
-    // filter categories, fetch and store
-    const categoryItems = headerMenu.items.filter(({ object }) => {
-      return object === 'category'
-    })
-    const categoryIds = categoryItems.map(({ object_id }) => object_id)
-    const categories = await dispatch('categories/fetchByIds', categoryIds, { root: true })
-    commit('SET_HEADER_CATEGORIES', categories)
+      /* Process header menu */
+      // get menu and add to store
+      const headerMenu = menus.find((menu) => menu.slug === HEADER_SLUG)
+      commit('SET_HEADER', headerMenu)
+      // filter categories, fetch and store
+      const categoryItems = headerMenu.items.filter(({ object }) => {
+        return object === 'category'
+      })
+      const categoryIds = categoryItems.map(({ object_id }) => object_id)
+      const categories = await dispatch('categories/fetchByIds', categoryIds, { root: true })
+      commit('SET_HEADER_CATEGORIES', categories)
 
-    const footerMenu = menus.find((menu) => menu.slug === FOOTER_SLUG)
-    commit('SET_FOOTER', footerMenu)
+      const footerMenu = menus.find((menu) => menu.slug === FOOTER_SLUG)
+      commit('SET_FOOTER', footerMenu)
 
-    return [headerMenu, footerMenu]
+      return [headerMenu, footerMenu]
+    }
   },
 
   setSelectedCategory({ commit }, category) {
