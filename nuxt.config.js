fcef6ecde30502598efdf6c976946cd4809bd600
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-05-19 19:39:16 +0200

    deployment : specify site language

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,9 @@ export default {
    ** Headers of the page
    */
   head: {
+    htmlAttrs: {
+      lang: 'fr'
+    },
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
