c4de1314a1c02ca5b024e750616d3bb98d27dcf1
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-05-23 11:10:17 +0200

    Layout : add cookieconsent to all layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,17 +7,29 @@
     <LayoutHeader />
     <nuxt />
     <LayoutFooter />
+    <cookie-consent>
+      <template slot="message">
+        <span class="text-white">
+          Ici, on aime les Cookies <i class="fas fa-cookie-bite"></i>, mais pas plus qu'il n'en faut !
+          <nuxt-link to="/politique-de-confidentialite" class="btn btn-outline-yellow mx-2">En savoir plus</nuxt-link>
+        </span>
+      </template>
+      <template slot="button">
+        <button class="btn btn-yellow mx-2">J'accepte</button>
+      </template>
+    </cookie-consent>
   </div>
 </template>
 
 <script>
+import CookieConsent from 'vue-cookieconsent-component'
 import LayoutHeaderCovid from '../components/LayoutHeaderCovid'
 import LayoutHeader from '../components/LayoutHeader'
 import LayoutFooter from '../components/LayoutFooter'
 import LayoutAlert from '../components/LayoutAlert'
 
 export default {
-  components: { LayoutAlert, LayoutHeader, LayoutFooter, LayoutHeaderCovid },
+  components: { LayoutAlert, LayoutHeader, LayoutFooter, LayoutHeaderCovid, CookieConsent },
   middleware({ store }) {
     if (process.client) {
       store.dispatch('nuxtServerInit')
