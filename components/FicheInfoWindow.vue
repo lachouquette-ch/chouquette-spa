2d481524c9cd9a563c1314abc63a7a422acf0768
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-08-10 16:00:54 +0200

    better resolution for fiche image

--- components/FicheInfoWindow.vue
+++ components/FicheInfoWindow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fiche-infowindow text-center">
     <h2 class="h4">{{ fiche.title.rendered | heDecode }}</h2>
-    <WpMedia v-if="featuredMedia" :media="featuredMedia" size="thumbnail" :no-src-set="true" class="" />
+    <WpMedia v-if="featuredMedia" :media="featuredMedia" size="medium" :no-src-set="true" class="" />
     <p class="my-2 muted">{{ address }}</p>
     <a v-if="fiche.info.chouquettise" :href="googleMapsURL" class="" title="Ouvrir avec Google maps" target="_blank">
       <i class="fas fa-map-marker-alt pr-1"></i> Ouvrir dans google maps
