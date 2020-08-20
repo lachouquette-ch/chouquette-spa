62ab4e1cc7357107fdf74f93f6fc44ec7fce3a86
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-08-20 20:58:34 +0200

    create new PostCardPlaceholder.vue component

---
+++ components/PostCardPlaceholder.vue
@@ -0,0 +1,21 @@
+<template>
+  <content-placeholders centered class="card post-card-placeholder">
+    <content-placeholders-img />
+    <content-placeholders-text :lines="1" />
+  </content-placeholders>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.post-card-placeholder {
+  width: 300px;
+  height: 300px;
+  padding: 10px;
+  margin: 30px;
+
+  .vue-content-placeholders-img {
+    height: 75%;
+  }
+}
+</style>
