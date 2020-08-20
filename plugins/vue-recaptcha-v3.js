df4331ce41990e5b2ae4ad7e68373ec11039d224
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-08-20 21:58:08 +0200

    development tip

--- plugins/vue-recaptcha-v3.js
+++ plugins/vue-recaptcha-v3.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import { VueReCaptcha } from 'vue-recaptcha-v3'
 
+// Dev tip : if component fails in a strange way with 'null' : it's probably the recaptcha configuration (on google admin) that doesn't allow this domain
 Vue.use(VueReCaptcha, {
   siteKey: process.env.recaptchaSiteKey,
   loaderOptions: {
