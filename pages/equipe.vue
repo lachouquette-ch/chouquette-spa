c49fab41b5403059cfd766e27b98355ab15b74aa
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-08-24 19:52:20 +0200

    equipe : migration to fetch with placeholders

--- pages/equipe.vue
+++ pages/equipe.vue
@@ -1,25 +1,29 @@
 <template>
   <WpPage :page="page">
     <template #footer>
-      <div v-if="team" class="my-3">
-        <h2 class="text-center mb-4">L'équipe</h2>
-        <div class="d-flex flex-wrap justify-content-around">
-          <div v-for="member in team" :key="member.id" class="card mr-3 mb-4">
-            <WpAvatar
-              :size="150"
-              :avatar-urls="member.avatar_urls"
-              :alt="member.name"
-              class="card-img-top mx-auto mt-3 rounded-circle"
-            />
-            <div class="card-body text-center">
-              <h3 class="card-title">{{ member.name }}</h3>
-              <p class="card-text small text-muted">{{ member.title }}</p>
-              <!-- eslint-disable-next-line vue/no-v-html -->
-              <p class="card-text" v-html="member.description" />
+      <template v-if="$fetchState.pending">
+        <b-spinner variant="yellow" label="chargement" class="text-center my-3"></b-spinner>
+      </template>
+      <template v-else>
+        <div class="my-3">
+          <div class="d-flex flex-wrap justify-content-around">
+            <div v-for="member in team" :key="member.id" class="card mr-3 mb-4">
+              <WpAvatar
+                :size="150"
+                :avatar-urls="member.avatar_urls"
+                :alt="member.name"
+                class="card-img-top mx-auto mt-3 rounded-circle"
+              />
+              <div class="card-body text-center">
+                <h3 class="card-title">{{ member.name }}</h3>
+                <p class="card-text small text-muted">{{ member.title }}</p>
+                <!-- eslint-disable-next-line vue/no-v-html -->
+                <p class="card-text" v-html="member.description" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </template>
     </template>
   </WpPage>
 </template>
@@ -33,6 +37,9 @@ export default {
     WpPage,
     WpAvatar,
   },
+  async fetch() {
+    this.team = await this.$wpAPI.wp.users.getTeam().then(({ data }) => data)
+  },
   async asyncData({ app }) {
     return {
       page: await app.$wpAPI.wp.pages.getBySlug('equipe').then(({ data }) => data[0]),
@@ -40,11 +47,8 @@ export default {
   },
   data() {
     return {
-      team: null,
+      team: [],
     }
-  },
-  async created() {
-    this.team = await this.$wpAPI.wp.users.getTeam().then(({ data }) => data)
   },
 }
 </script>
