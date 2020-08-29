e6733df8ab138ca412968550803d60fc575cc065
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-08-29 16:51:06 +0200

    universal

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     bannerLinkTo: process.env.BANNER_LINK_TO,
     bannerLinkText: process.env.BANNER_LINK_TEXT,
   },
-  mode: 'spa',
+  mode: 'universal',
   /*
    ** Headers of the page
    */
