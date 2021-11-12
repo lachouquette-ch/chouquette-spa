4336803327063b1ec83b2c33e62cb92375124af8
Fabrice Douchant <fabrice.douchant@gmail.com>
2021-11-12 16:05:30 +0100

    fix design

--- mixins/share.js
+++ mixins/share.js
@@ -9,7 +9,16 @@ export default {
   },
   methods: {
     shareWith(title, text, url) {
-      return navigator.share({ title, text, url })
+      try {
+        return navigator.share({ title, text, url })
+      } catch (e) {
+        // skip user abording share
+        if (e instanceof DOMException && e.code === DOMException.ABORT_ERR) {
+          console.log(e)
+        } else {
+          throw e
+        }
+      }
     },
   },
 }
