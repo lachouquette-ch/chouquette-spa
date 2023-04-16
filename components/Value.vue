98532c9253e3e200c9a3cd6602ecc88752c64861
Fabrice Douchant <fabrice.douchant@gmail.com>
2023-04-16 14:16:13 +0200

    add licence information to all headers

--- components/Value.vue
+++ components/Value.vue
@@ -1,3 +1,10 @@
+<!--
+  - Copyright (c) 2023 by Fabrice Douchant <fabrice.douchant@gmail.com>.
+  - This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+  - This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  - You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+  -->
+
 <template>
   <v-card color="white" class="text-center mx-auto py-5 d-flex flex-column" width="300" height="500">
     <v-card-title class="justify-center text-uppercase">
